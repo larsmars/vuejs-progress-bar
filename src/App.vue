80e7450df9feaecdf2cb4d4e2fd388cecc81f638
larsmars <lars@softwarefun.no>
2019-07-18 09:37:38 +0200

    Remove webpack use vue cli

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <progress-bar :value="25" />
+  </div>
+</template>
+
+<script>
+import ProgressBar from '../src/components/ProgressBar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ProgressBar
+  }
+}
+</script>
